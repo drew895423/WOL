2fff87541e2a83e1a4648f769ddb4739f4ca6b91
drew895423 <102764496+drew895423@users.noreply.github.com>
2022-05-12 15:00:47 -0400

    Init
    Wakes on lan

---
+++ WOL.ps1
@@ -0,0 +1,81 @@
+function Invoke-WakeOnLan
+{
+  param
+  (
+    # one or more MACAddresses
+    [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    # mac address must be a following this regex pattern:
+    [ValidatePattern('^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$')]
+    [string[]]
+    $MacAddress 
+  )
+ 
+  begin
+  {
+    # instantiate a UDP client:
+    $UDPclient = [System.Net.Sockets.UdpClient]::new()
+  }
+  process
+  {
+    foreach($_ in $MacAddress)
+    {
+      try {
+        $currentMacAddress = $_
+        
+        # get byte array from mac address:
+        $mac = $currentMacAddress -split '[:-]' |
+          # convert the hex number into byte:
+          ForEach-Object {
+            [System.Convert]::ToByte($_, 16)
+          }
+ 
+        #region compose the "magic packet"
+        
+        # create a byte array with 102 bytes initialized to 255 each:
+        $packet = [byte[]](,0xFF * 102)
+        
+        # leave the first 6 bytes untouched, and
+        # repeat the target mac address bytes in bytes 7 through 102:
+        6..101 | Foreach-Object { 
+          # $_ is indexing in the byte array,
+          # $_ % 6 produces repeating indices between 0 and 5
+          # (modulo operator)
+          $packet[$_] = $mac[($_ % 6)]
+        }
+        
+        #endregion
+        
+        # Define port and target IP address 
+        # Random here! 
+        for ($num = 6; $num -le 65000; $num++) {
+
+        [int] $Port = $num
+        $IP = "10.83.60.161" 
+        $Address = [system.net.IPAddress]::Parse($IP) 
+
+        # Create IP Endpoint 
+        $End = New-Object System.Net.IPEndPoint $address, $port 
+
+        $UDPclient.Connect($End)
+        
+        # send the magic packet to the broadcast address:
+        $null = $UDPclient.Send($packet, $packet.Length)
+        Write-Host "sent magic packet to $currentMacAddress and port $Port..."
+        }
+      }
+      catch 
+      {
+        Write-Warning "Unable to send ${mac}: $_"
+      }
+    }
+  }
+  end
+  {
+    # release the UDF client and free its memory:
+    $UDPclient.Close()
+    $UDPclient.Dispose()
+  }
+}
+
+$x = Read-Host('Enter the MAC address')
+Invoke-WakeOnLan($x);
