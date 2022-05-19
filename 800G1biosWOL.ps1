b738163a6d7e8f2a61baf6839f3fbb05aaa78936
drew895423 <102764496+drew895423@users.noreply.github.com>
2022-05-19 15:58:08 -0400

    Bios update through powershell
    Bios update through powershell

---
+++ 800G1biosWOL.ps1
@@ -0,0 +1,8 @@
+﻿$bios = Get-WmiObject -Namespace root/hp/instrumentedBIOS -Class HP_BIOSSettingInterface
+$bios.setbiossetting("Sunday", "Enabled")
+$bios.setbiossetting("Monday", "Enabled")
+$bios.setbiossetting("Tuesday", "Enabled")
+$bios.setbiossetting("Wednesday", "Enabled")
+$bios.setbiossetting("Thursday", "Enabled")
+$bios.setbiossetting("Friday", "Enabled")
+$bios.setbiossetting("Saturday", "Enabled")
